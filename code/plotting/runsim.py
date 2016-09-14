f8588d8238c299af2fb70b3c43c2c8b4dae23d3e
whitere123 <whitere@mcmaster.ca>
2016-09-14 19:06:12 -0400

    updating runsim

--- code/plotting/runsim.py
+++ code/plotting/runsim.py
@@ -1,9 +1,12 @@
+import sys
+sys.path.insert(0, './../models/')
+
 import EvoAlt8
 
 import importlib 
 reload(EvoAlt8) 
 
-rn=500000000
+rn=50000
 sd=[6,14,18,33] 
 freq=1000
 TT=1.1
@@ -24,7 +27,7 @@ ww=60
 
 x = EvoAlt8.runsim(a_range_init=(al,au),w=ww,R=RR,P=PP,T=TT,S=SS,roundnum=rn,seed=sd[0],rpt_freq=freq,CHECK=False,result_type='timeseries',mut_sd=(m1,m2,m3),ntype=nntype,mut_type=mtp,nsize=2,switch='off')
 print('yes')
-x1 = EvoAlt8.runsim(a_range_init=(al,au),w=ww,P=PP,R=RR,T=TT,S=SS,roundnum=rn,seed=sd[1],rpt_freq=freq,CHECK=False,result_type='timeseries',mut_sd=(m1,m2,m3),ntype=nntype,mut_type=mtp,nsize=10,switch='off')
+#x1 = EvoAlt8.runsim(a_range_init=(al,au),w=ww,P=PP,R=RR,T=TT,S=SS,roundnum=rn,seed=sd[1],rpt_freq=freq,CHECK=False,result_type='timeseries',mut_sd=(m1,m2,m3),ntype=nntype,mut_type=mtp,nsize=10,switch='off')
 print('yes')
 
 import matplotlib.pyplot as plt
@@ -39,11 +42,11 @@ plt.plot(x[:,4],label='std A(2)',color='yellow')
 #plt.plot(x[:,6],label='std Colour',color='orange')
 #plt.plot(x[:,7],label='std fitness',color='magenta')
 
-plt.plot(x1[:,0],label='mean(10)',linestyle='dashed',color='blue')
+#plt.plot(x1[:,0],label='mean(10)',linestyle='dashed',color='blue')
 #plt.plot(x1[:,1],linestyle='dashed',color='red')
 #plt.plot(x1[:,2],linestyle='dashed',color='black')
 #plt.plot(x1[:,3],linestyle='dashed',color='green')
-plt.plot(x1[:,4],linestyle='dashed',color='yellow')
+#plt.plot(x1[:,4],linestyle='dashed',color='yellow')
 #plt.plot(x1[:,5],linestyle='dashed',color='cyan')
 #plt.plot(x1[:,6],linestyle='dashed',color='orange')
 #plt.plot(x1[:,7],linestyle='dashed',color='magenta')
