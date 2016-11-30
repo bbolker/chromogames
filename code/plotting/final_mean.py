b7707c00834b6c3601e730c0db7bfec8de6c5897
whitere123 <whitere@mcmaster.ca>
2016-11-30 17:06:20 -0500

    testing equilibrium

---
+++ code/plotting/final_mean.py
@@ -0,0 +1,42 @@
+## Wed Dec 7 2016. Creating runmean for checking equulibrium of hawk-dove given differnet payoffs. 
+
+import sys
+sys.path.insert(0, './../models/')  # safer to do sys.path.append instead of insert.  
+import EvoAlt
+import numpy as np
+import importlib
+reload(EvoAlt) 
+
+rn=200000000
+sd=3
+freq=1000
+TT=1.7
+SS=0.3
+PP=0
+RR=1
+al=-2.1
+au=-2.0
+bl=0
+bu=0
+m1=0.02
+m2=0.02
+m3=0.02
+ns=1
+nntype='neu'
+mtp='add'
+ww=70
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
