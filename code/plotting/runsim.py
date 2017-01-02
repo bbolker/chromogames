eff997cc4dd4fea51e30e5ae009ff6d84f488454
whitere123 <whitere@mcmaster.ca>
2017-01-02 15:15:26 -0500

    new graphs

--- code/plotting/runsim.py
+++ code/plotting/runsim.py
@@ -7,32 +7,32 @@ reload(EvoAlt)
 
 outfiletest1 = open('test.npy','w')
 
-rn=250000000
+rn=50000000
 sd=[6,14,18,33] 
 freq=1000
 TT=1.7
 SS=0.3
 PP=0
 RR=1
-al=-4.6
-au=-4.5
+al=-1.4
+au=-1.3
 bl=0 
 bu=0
 m1=0.02
 m2=0.02
 m3=0.02 
-ns=1
+ns=4
 nntype='neu'
 mtp='add'
 ww=60
 
-x = EvoAlt.runsim(a_range_init=(al,au),w=ww,R=RR,P=PP,T=TT,S=SS,roundnum=rn,seed=sd[0],rpt_freq=freq,CHECK=False,result_type='timeseries',mut_sd=(m1,m2,m3),ntype=nntype,mut_type=mtp,nsize=1,switch='off')
+x = EvoAlt.runsim(a_range_init=(al,au),w=ww,R=RR,P=PP,T=TT,S=SS,roundnum=rn,seed=sd[0],rpt_freq=freq,CHECK=False,result_type='timeseries',mut_sd=(m1,m2,m3),ntype=nntype,mut_type=mtp,nsize=4,switch='off')
 
 np.save(outfiletest1,x) 
 
 import matplotlib.pyplot as plt
  
-plt.title('SD test2 w=60 mut=1% mutstd=0.02+')
+plt.title('SD nsize=4 test2 w=60 mut=1% mutstd=0.02+')
 plt.plot(x[:,0],label='mean A(2)', color='blue')
 #plt.plot(x[:,1],label='mean B', color='red')
 #plt.plot(x[:,2],label='mean colour',color='black')
