b6d637027c6b45c215c7630a33a6196519a07121
whitere123 <whitere@mcmaster.ca>
2017-03-06 21:54:04 -0500

    updated voter model

--- code/votermodel/fullstatesimC.py
+++ code/votermodel/fullstatesimC.py
@@ -1,10 +1,12 @@
 import numpy as np
 import votermodel as vm 
 
-roundnumb=3000 ## was 100000
+roundnumb=1000 ## was 100000
 time = 0.1
-r = vm.runvoter(roundnum=roundnumb,w=6, n_rpt=100, rng=[0,3],)
-
+res = vm.runvoter(roundnum=roundnumb,w=6, n_rpt=100, rng=[0,3],dist=2)
+r=res[0] 
+r2=res[1] 
+print(r2) 
 import matplotlib as mp
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -23,10 +25,10 @@ for i in range(int(roundnumb)):
 ani = animation.ArtistAnimation(fig,ims,interval=50,blit=True,repeat_delay=1000)
 
 #mp.rcParams['animation.bitrate'] = 100
-ani.save('new123.mp4',writer='ffmpeg')
-plt.show()
-
+ani.save('votermodel_3colors_6by6.mp4',writer='ffmpeg')
 plt.close() 
+plt.plot(r2,label='Spatial correlation distance 2')
+plt.show() 
  
 
 
--- code/votermodel/votermodel.py
+++ code/votermodel/votermodel.py
@@ -1,4 +1,3 @@
-
 import random,sys,time,math
 from random import randint
 import numpy
@@ -27,26 +26,51 @@ def find_nei(m,n,ntype,nsize,w):
         return neighbours
 
 
-def runvoter( roundnum=100000,w=4, n_rpt=10000, rng=[0,3], ):    
+def runvoter( roundnum=100000,w=4, n_rpt=10000, rng=[0,3]):    
         results = numpy.zeros((roundnum+1,w,w))
         votelst = numpy.random.randint(rng[0],rng[1],size=(w,w)) 
-		
+	correl=[[],[],[],[]]		
 	x = 0       
         #The game loop is started.
         while x <= roundnum:  
 		dc1 = numpy.random.randint(0,w)
         	dc2 = numpy.random.randint(0,w) 
 		neighbours = find_nei(dc1,dc2,'neu',1,w) 
-			
+		
+  		cor1=[] 
+		cor2=[] 
+		
+		for dist in range(1,5):		
+			for i in range(w):
+                        	for j in range(w):
+                                	cor1.append(votelst[i][j])  
+                                	cor1.append(votelst[i][j]) 
+                                	cor1.append(votelst[i][j]) 
+                                	cor1.append(votelst[i][j]) 
+                                	cor2.append(votelst[(i+dist)%w][j]) 
+                                	cor2.append(votelst[(i-dist)%w][j]) 
+                                	cor2.append(votelst[i][(j+dist)%w]) 
+                                	cor2.append(votelst[i][(j-dist)%w]) 
+			val=numpy.corrcoef(cor1,cor2) 
+			if (dist==1): 
+				correl[0].append(val) 
+			elif (dist==2):
+				correl[1].append(val) 
+			elif (dist==3): 
+				correl[2].append(val) 
+			else: 
+				correl[3].append(val) 
+			    
+	   
 		replace=randint(0,4)  
 		while (replace==2): 
 			replace=randint(0,4) 
 		
+		print(votelst) 		
 		votelst[dc1][dc2] = votelst[neighbours[replace][0]][neighbours[replace][1]] 
-		results[x]=votelst		
+		results[x]=votelst
 		x+=1 
 	
-	return results	 		
+	return [results,correl]	 		
 			
-runvoter()   
  
