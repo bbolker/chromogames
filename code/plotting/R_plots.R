b0965704b6e08fc2d294c66ddb2f781a04959abf
Ben Bolker <bbolker@gmail.com>
2017-03-29 17:28:28 -0400

    update R plots

--- code/plotting/R_plots.R
+++ code/plotting/R_plots.R
@@ -6,7 +6,7 @@ library(ggplot2)
 
 npy_dir <- "NPY_files/new"
 flist <- list.files(npy_dir,pattern="\\.npy$")
-flist <- flist[1:3]  ## temporarily skip last file - empty?
+flist <- flist[1:3]
 
 n0 <- outer(c("A","B","colour","fitness"),c("mean","sd"),paste,sep="_")
 
@@ -17,22 +17,32 @@ get_file <- function(f,path=npy_dir) {
     return(r)
 }
     
-alldat <- lapply(flist,get_file) %>%
-    setNames(flist) %>%
+alldat <- lapply(flist,get_file) %>%           ## get list of files
+    setNames(flist)
+## sapply(alldat,nrow)
+
+alldat2 <- alldat %>%                        ## set list names 
     bind_rows(.id="file") %>%                  ## combine all files
     mutate(file=gsub("\\.npy$","",file)) %>%   ## drop .npy extension
+    ## split filename (without extension) into three pieces of info
     separate(file,into=c("gametype","nbrsize","ic"))
 
-alldat2 <- alldat %>% select(gametype,nbrsize,ic,t,A_mean,A_sd)
+alldat3 <- alldat2 %>% select(gametype,nbrsize,ic,t,A_mean,A_sd)
 
-alldat3 <- alldat2 %>%
-    filter(t>50000) %>%
+alldat4 <- alldat3 %>%
     gather(var,val,c(A_mean,A_sd)) %>%  ## change to long format
-    group_by(gametype,nbrsize,ic,var) %>%       ## do some operation for each var
+    group_by(gametype,nbrsize,ic,var)
+
+ggplot(alldat4,aes(t,val,colour=nbrsize))+geom_line()+
+    facet_wrap(~var)+
+    scale_x_continuous(limits=c(0,50000))
+
+alldat5 <- alldat4 %>%       ## do some operation for each var
+    filter(t>max(t)-25000) %>%
     summarise(mean=mean(val),
               sd=sd(val))
 
-ggplot(alldat3,aes(nbrsize,mean))+
+ggplot(alldat4,aes(nbrsize,mean))+
     geom_pointrange(aes(ymin=mean-2*sd,ymax=mean+2*sd))+
     facet_wrap(~var,scale="free")
 
