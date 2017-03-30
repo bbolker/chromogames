a94912a652302501143a8c762bab671e6abb7c2e
Ben Bolker <bbolker@gmail.com>
2017-03-30 13:14:13 -0400

    update plotting code (move ns4 output)

--- code/plotting/R_plots.R
+++ code/plotting/R_plots.R
@@ -2,14 +2,12 @@ library(RcppCNPy)
 library(tidyr)
 library(dplyr)
 library(tibble)
-library(ggplot2)
+library(ggplot2); theme_set(theme_classic())
 
 npy_dir <- "NPY_files/new"
 flist <- list.files(npy_dir,pattern="\\.npy$")
-flist <- flist[1:3]
 
 n0 <- outer(c("A","B","colour","fitness"),c("mean","sd"),paste,sep="_")
-
 get_file <- function(f,path=npy_dir) {
     r <- npyLoad(file.path(path,f))  ## load file
     ## now add column names and a time column
@@ -33,18 +31,23 @@ alldat4 <- alldat3 %>%
     gather(var,val,c(A_mean,A_sd)) %>%  ## change to long format
     group_by(gametype,nbrsize,ic,var)
 
-ggplot(alldat4,aes(t,val,colour=nbrsize))+geom_line()+
-    facet_wrap(~var)+
-    scale_x_continuous(limits=c(0,50000))
+## we don't really need to plot every single point; thinning
+alldat4_thin <- alldat4 %>%
+    filter(t %% 100 == 0)
+
+ggplot(alldat4_thin,aes(t,val,colour=nbrsize))+geom_line()+
+    facet_wrap(~var)
+ggsave("ns_trajectories.pdf",width=8,height=5)
 
 alldat5 <- alldat4 %>%       ## do some operation for each var
     filter(t>max(t)-25000) %>%
     summarise(mean=mean(val),
               sd=sd(val))
 
-ggplot(alldat4,aes(nbrsize,mean))+
+ggplot(alldat5,aes(nbrsize,mean))+
     geom_pointrange(aes(ymin=mean-2*sd,ymax=mean+2*sd))+
-    facet_wrap(~var,scale="free")
-
+    facet_wrap(~var,scale="free")+
+    labs(x="neighborhood size",y="")
+ggsave("ns_summaries.pdf",width=8,height=5)
 
 
